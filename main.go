a554c9566e3ef9ba031717e4b9084f442aa3ebae
Max Base <MaxBaseCode@Gmail.Com>
2018-12-03 18:03:51 +0330

    Update main.go

--- main.go
+++ main.go
@@ -5,16 +5,31 @@ import (
 	"fmt"
 	"os"
 )
-
 func usage() {
 	fmt.Fprintf(os.Stderr," PHP Code Optimize\n\n")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
-
 func main() {
 	/*
 	arguments := flag.Args()
 	fmt.Println(arguments)
 	*/
+	flag.Usage = usage
+	InputFile := flag.String("input","","path of a file as input")
+	OutputFile := flag.String("output","","path of a file as output")
+	MinifyFile := flag.Bool("minify",true,"")
+	//OptimizeFile := flag.Bool("optimize",true,"")
+	flag.Parse()
+	/////////////////////////////////////////////////////////////////
+	if *InputFile == "" {
+		fmt.Println("Arguments is missing.\n");
+		usage();
+		os.Exit(1);
+	}
+	/////////////////////////////////////////////////////////////////
+	fmt.Println(*InputFile)
+	fmt.Println(*OutputFile)
+	fmt.Println(*MinifyFile)
+	//fmt.Println(*OptimizeFile)
 }
